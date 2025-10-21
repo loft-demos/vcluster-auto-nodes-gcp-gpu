32b67c2cbeade4d71c501567755a052d460b3137
Kurt Madel <kurt.madel@loft.sh>
2025-10-21 11:58:53 -0400

    Delete node/zones.tf

--- node/zones.tf
+++
@@ -1,9 +0,0 @@
-# Discover available zones in the region
-data "google_compute_zones" "available" {
-  region = local.region
-}
-
-# Pick one zone (either from input, or the first available)
-locals {
-  selected_zone = local.zone != "" ? local.zone : data.google_compute_zones.available.names[0]
-}
